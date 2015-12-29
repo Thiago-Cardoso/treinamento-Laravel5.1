6ace77e2a52f6594042d8a3f2fb574c35446cf6f
Thiago-Cardoso <trovador182@gmail.com>
2015-12-29 20:10:00 -0200

    Estrutura-Adicionar-Remover(ORM-Eloquent)

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,26 +1,115 @@
-<?php 
-	namespace estoque\Http\Controllers;
-	use Illuminate\Support\Facades\DB;
+<?php namespace estoque\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use estoque\Produto;
+use Request;
 
 	class ProdutoController extends Controller{
 
 
-
+		/*
 		public function lista()
 		{
 
-				$html = '<h1>Listagem de produtos com Laravel</h1>';
-				$html .= '<ul>';
-				$produtos = DB::select('select * from produtos');
+			$produtos = DB::select('select * from produtos');
 
-				foreach ($produtos as $p) {
-					$html .= '<li> Nome: '. $p->nome .',
-					Descrição: '. $p->descricao .'</li>';
+			return view('produto/listagem')->with('produtos', $produtos);
+			//return view('listagem')->withProdutos($produtos);
+			//return view('listagem', ['produtos' => $produtos]);
+		}*/
 
-					$html .= '</ul>';
-
-					return $html;
-				}
+		//com o Eloquent ORM
+		public function lista(){
+			$produtos = Produto::all();
+			return view('produto.listagem')->with('produtos', $produtos);
 		}
+
+		/*
+		public function mostra($id){
+		// retorna uma view com os detalhes
+			$resposta = DB::select('select * from produtos where id = ?',[$id]);
+
+			if(empty($resposta)) {
+				return "Esse produto não existe";
+			}
+			return view('produto/detalhes')->with('p', $resposta[0]);
+		}*/
+
+		//com o Eloquent ORM
+		public function mostra($id){
+			$produto = Produto::find($id);
+
+			if(empty($produto)) {
+				return "Esse produto não existe";
+			}
+			return view('produto.detalhes')->with('p', $produto);
+		}
+
+		public function novo()
+		{
+
+			return view('produto.formulario');
+		}	
+
+		/*
+		public function adiciona()
+		{
+				$nome = Request::input('nome');
+				$descricao = Request::input('descricao');
+				$valor = Request::input('valor');
+				$quantidade = Request::input('quantidade');
+
+				DB::table('produtos')->insert(
+				['nome' => $nome,
+				'valor' => $valor,
+				'descricao' => $descricao,
+				'quantidade' => $quantidade]);
+
+				//return view('produto.adicionado')->with('nome',$nome);
+				return redirect('/produtos')->withInput(Request::only('nome'));
+		}/*
+
+
+		/*
+		public function listaJson(){
+			$produtos = DB::select('select * from produtos');
+			return $produtos;
+		}*/
+
+		/*
+		public function listaJson()
+		{
+			$produtos = DB::select('select * from produtos');
+			return response()->json($produtos);
+		}*/
+
+		//com o Eloquent ORM
+		public function adiciona()
+		{		
+				/*
+				$params = Request::all();
+				$produto = new Produto($params);
+				$produto->save();*/
+				Produto::create(Request::all());
+
+				return redirect()
+					->action('ProdutoController@lista')
+					->withInput(Request::only('nome'));
+		}
+
+
+		//com o Eloquent ORM
+		public function listaJson(){
+			$produtos = Produto::all();
+			return response()->json($produtos);
+		}
+
+
+		public function remove($id){
+			$produto = Produto::find($id);
+			$produto->delete();
+			return redirect()
+			->action('ProdutoController@lista');
+	}
+
 
 }
--- app/produto.php
+++ app/produto.php
@@ -4,7 +4,11 @@ namespace estoque;
 
 use Illuminate\Database\Eloquent\Model;
 
-class produto extends Model
+class Produto extends Model
 {
-    //
+   protected $table = 'produtos';
+   public $timestamps = false;
+   protected $fillable = array('nome',
+	'descricao', 'valor', 'quantidade');
+   protected $guarded = ['id'];
 }
